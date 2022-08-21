4829a0e2281bc54804f6b2aff74f579189509344
AndreiGiurgiu99 <giurgiu65@gmail.com>
2022-08-21 22:47:21 +0300

    new

--- intrinsic.py
+++ intrinsic.py
@@ -4,11 +4,11 @@ import tkinter as tk
 from PIL import ImageTk, Image  
 import requests
 import pyEX as p
+from iexfinance.stocks import Stock
 
 #API
 
 token = "pk_4086bd94147b46e78abc8d392892e305"
-auth  = p.Client(api_token=token, version='stable')
 
 #create root window
 
@@ -20,24 +20,19 @@ root.title("Intrensic Value Calculator")
 
 root.config(bg="#ccccca")
 
-#import img
-
-# bg_img = Image.open("dollar-black-circle-icon-28.png")
-# bg_img2 = bg_img.resize((50,50), Image.ANTIALIAS)
-# bg_img_tk = ImageTk.PhotoImage(bg_img2)
-
-
-
 #Create start frame with buttons for the other menus
-frm_start = tk.Frame(root, width=400, height=500, bg='#ccccca')
-frm_start.grid(row=0,column=0)
+frm_start = tk.Frame(root, width=500, height=500, bg='#ccccca')
+frm_start.grid(row=0,column=0,pady=5,padx=10)
 
 #Create frame, grid and widgets
-frm = tk.Frame(root, width=400, height=500, bg="#ccccca",pady=50)
-
+frm = tk.Frame(root, width=500, height=500, bg="#ccccca",pady=10)
 frm.grid(column=0,row=0,pady=5,padx=10)
 
-#img_label = tk.Label(image=bg_img_tk).grid(row=1,column=0)
+#Create calculator frame
+frm_calc = tk.Frame(root,width=500,height=500,bg='#ccccca')
+frm_calc.grid(column=0,row=0,pady=5,padx=10)
+
+
 
 #Functie care face get request pentru datele de pe site 
 def getTickerInfo():
@@ -77,32 +72,63 @@ def DeleteValues():
 def backToMenu():
     frm.grid_forget()
     frm_start.grid()
+
+def backToMenuCalc():
+    frm_calc.grid_forget()
+    frm_start.grid()
     
     
 def SwitchStockInfo():
     frm_start.grid_forget()
     frm.grid()
 
+def SwitchToCalc():
+    frm_start.grid_forget()
+    frm_calc.grid()
+
+def IntrensicValue():
+    valoare = (int(calc_eps.get())*(7+int(calc_growth.get()))*8.5)/4.4
+    calc_value.insert(0, valoare)
 
 #start page buttons
-start_label = tk.Label(frm_start,bg="#ccccca",text="Welcome to my app :)",font="Roboto").grid(row=1,column=2,padx=75,pady=25)
+start_label = tk.Label(frm_start,bg="#ccccca",text="Welcome to my app :)",font="Roboto").grid(row=1,column=2,padx=50,pady=25)
 
 basic_info = tk.Button(frm_start,bg="#ccccca",text="Stock Info",font="Roboto", command=SwitchStockInfo)
 basic_info.grid(row=2,column=2,pady=5)
 
-calculator = tk.Button(frm_start,bg="#ccccca",text="Calculator",font="Roboto", command=SwitchStockInfo)
+calculator = tk.Button(frm_start,bg="#ccccca",text="Calculator",font="Roboto", command=SwitchToCalc)
 calculator.grid(row=3,column=2,pady=5)
 
 help_btn = tk.Button(frm_start,bg="#ccccca",text="Help",font="Roboto", command=SwitchStockInfo)
 help_btn.grid(row=4,column=2,pady=5)
 
+#Calculator 
 
+calc_label = tk.Label(frm_calc, bg = "#ccccca", text = "Please get the necessary information \n from YahooFinance", justify=CENTER).grid(row=1,column=2,padx=5,pady=10)
+
+calc_eps_label = tk.Label(frm_calc,bg = "#ccccca", text = "EPS", justify=CENTER).grid(row=2,column=1,padx=5,pady=3)
+calc_growth_label = tk.Label(frm_calc,bg = "#ccccca", text = "Growth Rate", justify=CENTER).grid(row=3,column=1,padx=5,pady=3)
+calc_value = tk.Label(frm_calc,bg = "#ccccca", text = "Intrensic Value", justify=CENTER).grid(row=4,column=1,padx=5,pady=3)
+
+calc_eps = tk.Entry(frm_calc, justify=CENTER)
+calc_eps.grid(row=2,column=2,padx=5,pady=10)
+
+calc_growth = tk.Entry(frm_calc, justify=CENTER)
+calc_growth.grid(row=3,column=2,padx=5,pady=10)
+
+calc_value = tk.Entry(frm_calc, bg = "#ccccca",justify=CENTER)
+calc_value.grid(row=4,column=2,padx=5,pady=10)
+
+calc_btn_valoare = tk.Button(frm_calc,text='Value',command=IntrensicValue)
+calc_btn_valoare.grid(row=5,column=2)
+calc_btn_goback = tk.Button(frm_calc,text ='Back',command=backToMenuCalc)
+calc_btn_goback.grid(row=5,column=1)
 
 #Ticker Name Widget
 
 ticker_name_label = tk.Label(frm,bg="#ccccca",text="Ticker",font="Roboto").grid(row=2,column=1,padx=5)
 
-ticker_name = tk.Entry(frm)
+ticker_name = tk.Entry(frm,justify=CENTER)
 
 ticker_name.grid(row=2,column=2)
 
@@ -162,7 +188,11 @@ ticker_btn2 = tk.Button(frm, text='Delete values', command=DeleteValues)
 back_btn =tk.Button(frm,text='Back',command=backToMenu)
 back_btn.grid(row=9,column=1,padx=5,pady=5)
 
+#show only main page
 frm.grid_forget()
+frm_calc.grid_forget()
+
+
 root.mainloop() #makes eveything apper
 
 
